02cbb3035f3d775ca999fc516374f9d391ab62b5
ediardor.contato@outlook.com <eduardor.contato@outlook.com>
2021-03-11 01:57:30 -0300

    Inital Strapi configs

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a790a9da5edcd1544674c3ca0750f4a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2c789da7-a1f8-445d-aff4-d91d1d140b88'
+};
