b7db68e264cc807e0f6a4547011548d745f8a0d6
ediardor.contato@outlook.com <eduardor.contato@outlook.com>
2021-03-28 03:19:06 -0300

    Fixed Heroku ENV VARS (local DB password)

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi123'),
+        password: env('DATABASE_PASSWORD', '125634'),
         ssl: {
           rejectUnauthorized: false
         }
